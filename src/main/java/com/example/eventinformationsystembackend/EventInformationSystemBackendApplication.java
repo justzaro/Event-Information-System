d9c4db232d76e6bf881e6db19aac7e15bd46de06
justzaro <zarocuxx@yahoo.com>
2023-07-22 17:35:30 +0300

    initial commit

---
+++ src/main/java/com/example/eventinformationsystembackend/EventInformationSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.eventinformationsystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventInformationSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventInformationSystemBackendApplication.class, args);
+	}
+
+}
